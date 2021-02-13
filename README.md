f49dc9940f4351b9b2050300b7ac4280a0ce4206
Preyash Patel <preyash2047@GMAIL.com>
2021-02-13 21:21:32 +0530

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfolio-project
- Using Django i have developed this portfolio website containing Blog, Jobs and Certificates
--- certi/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Certi
-
-admin.site.register(Certi)
